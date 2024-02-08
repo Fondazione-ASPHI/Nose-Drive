33b506d9b1b50040a4780ba36103f35b74458e54
Alessandro Norfo <anorfo@asphi.it>
2024-02-08 15:50:30 +0100

    .

--- eyeware_beam/Eye_Mouse.py
+++ eyeware_beam/Eye_Mouse.py
@@ -21,7 +21,13 @@ while True:
   screen_gaze = tracker.get_screen_gaze_info()
   screen_gaze_is_lost = screen_gaze.is_lost
   # pyautogui.moveTo(screen_gaze.x, screen_gaze.y)
-  win32api.mouse_event(win32con.MOUSEEVENTF_MOVE | win32con.MOUSEEVENTF_ABSOLUTE, int(65536/1920*screen_gaze.x), int(65536/1080*screen_gaze.y), 0, 0)
+
+  mult_w = 65536/width
+  mult_h = 65536/height
+  gaze_x = int(mult_w*screen_gaze.x)
+  gaze_y = int(mult_h*screen_gaze.y)
+  
+  win32api.mouse_event(win32con.MOUSEEVENTF_MOVE | win32con.MOUSEEVENTF_ABSOLUTE, gaze_x, gaze_y, 0, 0)
 
 
 
