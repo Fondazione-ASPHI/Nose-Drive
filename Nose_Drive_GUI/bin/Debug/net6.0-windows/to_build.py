620d0bbdf4a0358bcfab7e99a9d7887af6c8579a
Alessandro Norfo <sprintingkiwi@gmail.com>
2024-07-23 08:40:14 +0200

    .

--- Nose_Drive_GUI/bin/Debug/net6.0-windows/to_build.py
+++ Nose_Drive_GUI/bin/Debug/net6.0-windows/to_build.py
@@ -1,120 +1,62 @@
+####
+# THIS SCRIPT is aimed at moving a character in 3rd or 1st person videogames such as Skyrim.
+# Nose up down moves the character, Nose right left moves the camera
+# The rest is up to vocal commands
+# Alternatively you can use the "Head Mouse" script to move camera while moving the character with vocal commands
+###
+
 import sys
 options_file_name = sys.argv[1]
 
-import sys
-key_list = sys.argv[2]
-
 import json
 from nosedrive import track_face
+import keyboard as kb
 
 # Init Gamepad controller
 import vgamepad as vg # https://pypi.org/project/vgamepad/   https://github.com/ViGEm/ViGEmBus
 gamepad = vg.VX360Gamepad()
 
 
-tasti = {
-  "A": vg.XUSB_BUTTON.XUSB_GAMEPAD_A,
-  "B": vg.XUSB_BUTTON.XUSB_GAMEPAD_B,
-  "X": vg.XUSB_BUTTON.XUSB_GAMEPAD_X,
-  "Y": vg.XUSB_BUTTON.XUSB_GAMEPAD_Y,
-  "LB": vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER,
-  "RB": vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER,
-  "Start": vg.XUSB_BUTTON.XUSB_GAMEPAD_START,
-  "Select": vg.XUSB_BUTTON.XUSB_GAMEPAD_BACK,
-  "Dpad Right": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_RIGHT,
-  "Dpad Left": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_LEFT,
-  "Dpad Up": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_UP,
-  "Dpad Down": vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_DOWN,
-  "Right Thumb": vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_THUMB,
-  "Left Thumb": vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_THUMB,
-  "Home": vg.XUSB_BUTTON.XUSB_GAMEPAD_GUIDE,
-}
-#tasti["A"]
+#################
+# CUSTOM LOGIC #
+#################
+mode = "Move" # or "Aim"
 
-joyfunctions = {
-  "Left": gamepad.left_joystick_float,
-  "Right": gamepad.right_joystick_float
-}
+def logic(nose_x, nose_y, mouth_x, head_tilt, trigger_eyebrows, trigger_mouth_open):  
+  global mode
+  x, y = 0, 0
 
-triggerfunctions = {
-  "LT": gamepad.left_trigger_float,
-  "RT": gamepad.right_trigger_float
-}
+  if kb.is_pressed("8"):
+    mode = "Move"
+  elif kb.is_pressed("9"):
+    mode = "Aim"
 
+  if mode == "Move":
+    # Camera with HORIZONTAL NOSE
+    if nose_x < -0.2 or nose_x > 0.2: # Left
+      gamepad.right_joystick_float(x_value_float=nose_x, y_value_float=0)
 
-##################################
-# IMPORT OPTIONS + Selected KEYS #
-##################################
-options_file = open(options_file_name) # Opening JSON file
-options = json.load(options_file) # returns JSON object as a dictionary
-options_file.close() # Closing file
+    # Movement with VERTICAL NOSE
+    gamepad.left_joystick_float(x_value_float=0, y_value_float=nose_y)
 
-key_import = open(key_list)
-keyBinds = json.load(key_import) # returns JSON object as a dictionary
-key_import.close() # Closing file
+  elif mode == "Aim":    
+    if nose_x < -0.2 or nose_x > 0.2: # Left
+      x = nose_x
+    if nose_y < -0.2 or nose_y > 0.2: # Right
+      y = nose_y
+    gamepad.right_joystick_float(x_value_float=x, y_value_float=y)
 
-
-
-########
-# LOGIC #
-########
-def logic(nose_x, nose_y, mouth_x, head_tilt, trigger_eyebrows, trigger_mouth_open):
-
-  zona_morta = 0
-
-  analogMovements = {
-    "nose_right": -nose_x,
-    "nose_left": nose_x,
-    "nose_up": -nose_y,
-    "nose_down": nose_y,
-    "mouth_right": -mouth_x,
-    "mouth_left": mouth_x
-  }
-
-  xy = [0, 0]
-  directions = {
-    "Right": 0,
-    "Left": 0,
-    "Up": 1,
-    "Down": 1
-  }
-
-
-  for movement in keyBinds:
-
-    value = keyBinds[movement]
-
-
-    # JOYSTICKS
-    if "Joystick" in value:
-      keywords = value.split(" ")
-      joyside = keywords[0]
-      direction = keywords[2]
-      if movement in analogMovements:
-        xy[directions[direction]] = analogMovements[movement]
-        # print(xy)
-      joyfunctions[joyside](x_value_float=xy[0], y_value_float=xy[1])
-
-
-    # BUTTONS
-    if value in tasti:
-      comando = tasti[value]
-      if movement in analogMovements:
-        if analogMovements[movement] > 0.5:                  
-          gamepad.press_button(button=comando)
-        else:
-          gamepad.release_button(button=comando)
-
-      
-    # TRIGGERS
-    if "LT" in value or "RT" in value:
-      triggerfunctions[value](analogMovements[movement])
-
-
-  # UPDATE
   gamepad.update()
   gamepad.reset()
 
+
+
+#################
+# IMPORT OPTIONS #
+#################
+options_file = open(options_file_name) # Opening JSON file
+options = json.load(options_file) # returns JSON object as a dictionary
+options_file.close() # Closing file
 
 
 #################
